1527b0aedf5f4fef5b5cf18e4b130e32c2fe838f
angel paul <angelpaula155@gmail.com>
2026-04-22 21:54:46 +0530

    Day 3 — Describe API working

--- ai-service/app.py
+++ ai-service/app.py
@@ -1,6 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask
+import routes.describe
 
 app = Flask(__name__)
+
+app.register_blueprint(routes.describe.describe_bp)
+
+print(app.url_map)
 
 @app.route("/")
 def home():
